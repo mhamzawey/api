623135a96237dad427dee904e4cb95bc210a191f
mhamzawey <mac@Macs-Mac-mini.local>
2018-10-19 17:42:58 +0200

    adding event model to events/models.py

--- events/models.py
+++ events/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(null=True, blank=True, max_length=150)
+    description = models.CharField(null=True, blank=True, max_length=225)
+    category = models.CharField(null=True, blank=True, max_length=80)
+    start_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
+    link = models.URLField(null=True, blank=True,max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "Events"
+        ordering = ['title']
+
