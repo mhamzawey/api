18bcb3550b731afddb76c4fd25127a07621a0ffb
mhamzawey <mac@Macs-Mac-mini.local>
2018-10-20 15:01:45 +0200

    modifying the models for start and end dates to be charfield and adding this to the serializers

--- events/serializers.py
+++ events/serializers.py
@@ -4,6 +4,15 @@ from rest_framework import serializers
 
 
 class EventSerializer(serializers.ModelSerializer):
+    title = serializers.CharField(max_length=150)
+    description = serializers.CharField(max_length=225)
+    category = serializers.CharField(max_length=80)
+    start_date = serializers.CharField(max_length=80)
+    end_date = serializers.CharField(max_length=80)
+    link = serializers.URLField(max_length=128)
+
+
     class Meta:
         model = Event
-        fields = "__all__"
+        fields = ('id','title', 'description','category','start_date','end_date','link','created_at','updated_at')
+
